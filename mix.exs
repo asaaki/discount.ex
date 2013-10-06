7bc8bb581435e18bd003cc1a7cc2fd2b24323c9b
Christoph Grabo <chris@dinarrr.com>
2013-10-06 07:35:48 +0200

    Update project part in mix file

--- mix.exs
+++ mix.exs
@@ -13,7 +13,9 @@ defmodule Discount.Mixfile do
     [ app: :discount,
       version: "0.0.1",
       elixir: "~> 0.10.3",
-      deps: deps ]
+      compilers: [:discount, :elixir, :app],
+      source_url: "https://github.com/asaaki/discount.ex"
+    ]
   end
 
   # Configuration for the OTP application
@@ -21,9 +23,4 @@ defmodule Discount.Mixfile do
     [mod: { Discount, [] }]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
-  defp deps do
-    []
-  end
 end
