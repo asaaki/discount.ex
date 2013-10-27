d0867e6e3f720bcd71837d83f83b8a5a9fd4f11b
Christoph Grabo <chris@dinarrr.com>
2013-10-27 20:33:20 +0100

    Bump version to 0.2.1

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(
   name:        "discount",
   description: "Discount.ex - Elixir wrapper for discount, a Markdown parser",
-  version:     "0.2.0",
+  version:     "0.2.1",
 
   keywords:     [
                   "markdown",
@@ -16,7 +16,7 @@ Expm.Package.new(
                   ]
                 ],
   repositories: [
-                  [ github: "asaaki/discount.ex", tag: "0.2.0" ]
+                  [ github: "asaaki/discount.ex", tag: "0.2.1" ]
                 ],
   dependencies: [
                 ])
