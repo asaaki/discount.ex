caea67d79e7f5cc839a57a0399cb3ee7097b66e8
Christoph Grabo <chris@dinarrr.com>
2013-10-28 16:35:07 +0100

    Add more type checks for input data

--- lib/discount.ex
+++ lib/discount.ex
@@ -4,7 +4,7 @@ defmodule Discount do
     Discount.Markdown.parse_doc_list input_data, callback
   end
 
-  def to_html(input_data, callback) do
+  def to_html(input_data, callback) when is_bitstring(input_data) do
     Discount.Markdown.parse_doc input_data, callback
   end
 
@@ -12,7 +12,7 @@ defmodule Discount do
     Discount.Markdown.parse_doc_list input_data
   end
 
-  def to_html(input_data) do
+  def to_html(input_data) when is_bitstring(input_data) do
     Discount.Markdown.parse_doc(input_data)
   end
 
